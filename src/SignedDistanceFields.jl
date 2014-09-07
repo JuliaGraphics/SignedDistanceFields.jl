fada8c674eb3ee75438e68f41a50d80f44e8e406
Yuri Vishnevsky <yurivish@gmail.com>
2014-09-07 11:20:10 -0400

    Initial pass at a working implementation of Saito's algorithm

--- src/SignedDistanceFields.jl
+++ src/SignedDistanceFields.jl
@@ -1,34 +1,6 @@
 module SignedDistanceFields
 
-using LowDimNearestNeighbors
 using Images, Color, FixedPointNumbers
-
-# Define multidimensional vector types for testing
-immutable Vec2{T <: Unsigned}
-	x::T
-	y::T
-end
-Vec2(x, y) = Vec2(convert(Unsigned, x), convert(Unsigned, y))
-Base.getindex(v::Vec2, n::Int) = n == 1 ? v.x : n == 2 ? v.y : throw("Vec2 indexing error.")
-Base.length(v::Vec2) = 2
-Base.rand{T}(::Type{Vec2{T}}) = Vec2(rand(T), rand(T))
-
-
-path = "/Users/yurivish/Desktop/a.png"
-I = 1273
-J = 1250
-
-path = "/Users/yurivish/Desktop/a_medium.png"
-I = 509
-J = 500
-
-path = "/Users/yurivish/Desktop/a_smaller.png"
-I = 306
-J = 300
-
-path = "/Users/yurivish/Desktop/a_small.png"
-I = 102
-J = 100
 
 function sqdist1d(vec::Vector{Bool})
 	# Sweep forward; then sweep back.
@@ -57,76 +29,98 @@ function sqdist1d(vec::Vector{Bool})
 end
 
 function sqdist2d(arr)
-	dists2 = fill(typemax(Int), size(arr))
+	cdists = mapslices(sqdist1d, arr, 1)
+	rdists = fill(typemax(Int) >> 2, size(arr))
 
-	dists = mapslices(sqdist1d, arr, 1)
 	println("Column distances:")
-	println(dists)
+	println(cdists)
 	
-    for i = 1:size(dists,1)
-		for j = 1:size(dists,2)
-			for k = 1:size(dists,2)
-		        dists2[i, j] = min(dists[i, j], dists[i, k] + (j - k)^2)
+    for i = 1:size(cdists, 1)
+		for j = 1:size(cdists, 2)
+			for k = 1:size(cdists, 2)
+		        rdists[i, j] = min(rdists[i, j], cdists[i, k] + (j - k)^2)
 			end
 		end
     end
 
 	println("Sqdist2d:")
-    dists2
+    rdists
+end
+
+function sqdistancetransform(img)
+	maxval = typemax(Int) >> 2
+	distsq = fill(maxval, size(img))
+
+	for r = 1:size(img, 2)
+		 d = -1
+		 for c in 1:size(img, 1)
+		 	val = img[c, r]
+		 	d < 0 && !val && continue
+		 	d = val ? 0 : d + 1
+		 	distsq[c, r] = d^2
+		 end
+
+		 d = -1
+		 for c in size(img, 1):-1:1
+		 	val = img[c, r]
+		 	d < 0 && !val && continue
+		 	d = val ? 0 : d + 1
+		 	distsq[c, r] = min(distsq[c, r], d^2)
+		 end
+	end
+
+	all = fill(maxval, size(img))
+
+    for i = 1:size(distsq, 1)
+		for j = 1:size(distsq, 2)
+			for k = 1:size(distsq, 2)
+		        all[i, j] = min(all[i, j], distsq[i, k] + (j - k)^2)
+			end
+		end
+    end
+
+    all
 end
 
 # row = [false, false, false, false, false, false, false, false, true, true, false, true];
 # sqdist1d(row)
 mat = [
-	true false false;
 	false false false;
-	false false true
+	true true false;
 ];
 
 println("Matrix:")
 mat
-sqdist2d(mat)
+sqdistancetransform(mat)
 
 
 # TODO: Determine which orientation (rc, cr) is better for cache locality
 
 
+path = "/Users/yurivish/Desktop/a.png"
+# path = "/Users/yurivish/Desktop/a_medium.png"
+# path = "/Users/yurivish/Desktop/a_smaller.png"
+# path = "/Users/yurivish/Desktop/a_small.png"
+
 
 img = convert(Array, imread(path))
+# println(img)
+bitmap = map(c -> c.c.r == 1 && c.c.g == 1 && c.c.b == 1, img)
 
-chan = img[:, :, 1] .< 0xff
+# imwrite(map(b -> RGB(b, b, b), bitmap), "out_b.png")
 
-count(f-> f, chan)
-count(f-> !f, chan)
+distancetransform(img) = sqrt(sqdistancetransform(img))# - sqdistancetransform(!img))
 
-filled = Vec2{Uint8}[]
+# println(bitmap)
+@time result = distancetransform(bitmap)
+# println(result)
+# result = sqrt(result) # type-unstable
+result /= maximum(result)
 
-for i=1:I,j=1:J
-	chan[i,j] && push!(filled, Vec2{Uint8}(i, j))
-end
+# println(result[1:10])
+# println(result)
+# println(summary(result))
 
-println(length(filled))
-
-preprocess!(filled)
-
-function fn(i, j)
-	i == 1 && println("fn($i, $j)")
-	a = Vec2(i, j)
-	b = nearest(filled, a)
-	sqrt((a[1] - b[1])^2 + (a[2] - b[2])^2)
-end
-
-@time result = [fn(i, j)::Float64 for i=1:I, j=1:J]
-
-# result = clamp(result, 0, 20)
-# result /= maximum(result)
-
-println(result[1:10])
-println(summary(result))
-
-# imwrite(float(chan), "out.png")
-imwrite(result, "out.png")
-
-# println(sort(vec(result)))
+imwrite(map(b -> RGB(b, b, b), result), "out.png")
 
 end # module
