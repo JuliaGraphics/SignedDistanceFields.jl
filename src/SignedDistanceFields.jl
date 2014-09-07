9f2691bc9a2dae89ef136f020bdd8b24b388e1e5
Yuri Vishnevsky <yurivish@gmail.com>
2014-09-07 11:36:19 -0400

    Things appear to work. Implemented a get-out-early optimization; there are more to do (such as not sweeping from left to right but in widening oscillation, and thinking through rows and columns, and having a general way of turning an image into a bitmap)

--- src/SignedDistanceFields.jl
+++ src/SignedDistanceFields.jl
@@ -2,51 +2,6 @@ module SignedDistanceFields
 
 using Images, Color, FixedPointNumbers
 
-function sqdist1d(vec::Vector{Bool})
-	# Sweep forward; then sweep back.
-	# TODO: Investigate immediate sweepback
-	# as far back as required; better cache
-	# locality that way.
-	distsq = fill(typemax(Int) >> 2, size(vec)) # Choose the largest value s.t. it squared + c < typemax
-
-	d = -1
-	for i in 1:length(vec)
-		val = vec[i]
-		d < 0 && !val && continue
-		d = val ? 0 : d + 1
-		distsq[i] = d^2
-	end
-
-	d = -1
-	for i in length(vec):-1:1
-		val = vec[i]
-		d < 0 && !val && continue
-		d = val ? 0 : d + 1
-		distsq[i] = min(distsq[i], d^2)
-	end
-
-	distsq
-end
-
-function sqdist2d(arr)
-	cdists = mapslices(sqdist1d, arr, 1)
-	rdists = fill(typemax(Int) >> 2, size(arr))
-
-	println("Column distances:")
-	println(cdists)
-	
-    for i = 1:size(cdists, 1)
-		for j = 1:size(cdists, 2)
-			for k = 1:size(cdists, 2)
-		        rdists[i, j] = min(rdists[i, j], cdists[i, k] + (j - k)^2)
-			end
-		end
-    end
-
-	println("Sqdist2d:")
-    rdists
-end
-
 function sqdistancetransform(img)
 	maxval = typemax(Int) >> 2
 	distsq = fill(maxval, size(img))
@@ -74,7 +29,9 @@ function sqdistancetransform(img)
     for i = 1:size(distsq, 1)
 		for j = 1:size(distsq, 2)
 			for k = 1:size(distsq, 2)
-		        all[i, j] = min(all[i, j], distsq[i, k] + (j - k)^2)
+				rowdist = (j - k)^2
+				rowdist > all[i, j] && break
+		        all[i, j] = min(all[i, j], distsq[i, k] + rowdist)
 			end
 		end
     end
@@ -82,44 +39,28 @@ function sqdistancetransform(img)
     all
 end
 
-# row = [false, false, false, false, false, false, false, false, true, true, false, true];
-# sqdist1d(row)
-mat = [
-	false false false;
-	true true false;
-];
-
-println("Matrix:")
-mat
-sqdistancetransform(mat)
-
-
 # TODO: Determine which orientation (rc, cr) is better for cache locality
 
-
-path = "/Users/yurivish/Desktop/a.png"
-# path = "/Users/yurivish/Desktop/a_medium.png"
-# path = "/Users/yurivish/Desktop/a_smaller.png"
-# path = "/Users/yurivish/Desktop/a_small.png"
-
+path = "/Users/yurivish/Desktop/a_1000.jpg"
+# path = "/Users/yurivish/Desktop/a_500.jpg"
 
 img = convert(Array, imread(path))
-# println(img)
-bitmap = map(c -> c.c.r == 1 && c.c.g == 1 && c.c.b == 1, img)
 
-# imwrite(map(b -> RGB(b, b, b), bitmap), "out_b.png")
+println("img[1] = ", img[1])
+# bitmap = map(c -> !(c.c.r == 1 && c.c.g == 1 && c.c.b == 1), img)
+bitmap = map(c -> c != 1, img)
 
-distancetransform(img) = sqrt(sqdistancetransform(img))# - sqdistancetransform(!img))
+# TODO: Can we calculate the SDF in one pass rather than two?
+# TODO: + -> -
+sdf(img) = sqrt(sqdistancetransform(img)) - sqrt(sqdistancetransform(!img))
 
 # println(bitmap)
-@time result = distancetransform(bitmap)
-# println(result)
-# result = sqrt(result) # type-unstable
-result /= maximum(result)
+@time result = sdf(bitmap)
 
-# println(result[1:10])
-# println(result)
-# println(summary(result))
+# Rescale to [0, 1]
+result = abs(result)
+result -= minimum(result)
+result /= maximum(result)
 
 imwrite(map(b -> RGB(b, b, b), result), "out.png")
 
