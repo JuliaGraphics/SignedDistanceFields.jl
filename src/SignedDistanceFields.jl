e261ae69aad9cb3056ed28fbc491f8c27a090f74
Yuri Vishnevsky <yurivish@gmail.com>
2014-09-08 19:24:42 -0400

    Name the results of size(img) nrows and ncols

--- src/SignedDistanceFields.jl
+++ src/SignedDistanceFields.jl
@@ -28,21 +28,22 @@ end
 function edf_sq(img)
 	# An upper bound for the distance between two pixels
 	maxval = prod(size(img))^2
+	ncols, nrows = size(img)
 
 	# Calculate the row-wise distance transform for each row
 	# in two passes, taking the minimum of the distance-from-
 	# left and distance-from-right.
 	rowdf_sq = fill(maxval, size(img))
-	for y in 1:size(img, 1)
-		xsweep!(img, rowdf_sq, y, 1:size(img, 2))
-		xsweep!(img, rowdf_sq, y, reverse(1:size(img, 2)))
+	for y in 1:ncols
+		xsweep!(img, rowdf_sq, y, 1:nrows)
+		xsweep!(img, rowdf_sq, y, reverse(1:nrows))
 	end
 
 	# Use the row-wise information to compute the full distance transform
 	df_sq = fill(maxval, size(img))
-	for x in 1:size(img, 2)
-		for y in 1:size(img, 1)
-			for yp in 1:size(img, 1)
+	for x in 1:nrows
+		for y in 1:ncols
+			for yp in 1:ncols
 				ydistsq = (y - yp)^2
 				ydistsq > df_sq[y, x] && break
 				df_sq[y, x] = min(df_sq[y, x], rowdf_sq[yp, x] + ydistsq)
